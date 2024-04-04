ce14ec0c7e4e12285e5473f9104ccd6f98fbc7cc
merouane meriem <lm_merouane@esi.dz>
2024-04-04 16:39:41 +0200

    Update urls.py

--- Backend/accounts/urls.py
+++ Backend/accounts/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import RegisterView, LoginUserView, LogoutAPIView, AddUserView, ListUsersView, DeleteUserView, \
     PasswordResetRequestView, PasswordResetConfirm, SetNewPasswordView
 
-from rest_framework_simplejwt.views import (TokenRefreshView,)
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
@@ -15,5 +15,5 @@ urlpatterns = [
     path('password-reset/', PasswordResetRequestView.as_view(), name='password-reset'),
     path('password-reset-confirm/<uidb64>/<token>/', PasswordResetConfirm.as_view(), name='reset-password-confirm'),
     path('set-new-password/', SetNewPasswordView.as_view(), name='set-new-password'),
-
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     ]
