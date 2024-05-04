88175618e993521f376b4c2972323e7635f004e3
merouane meriem <lm_merouane@esi.dz>
2024-05-04 16:31:45 +0200

    Update urls.py

--- Backend/accounts/urls.py
+++ Backend/accounts/urls.py
@@ -1,7 +1,8 @@
 from unicodedata import name
 from django.urls import path
 from .views import RegisterView, LoginUserView, LogoutAPIView, AddUserView, ListUsersView, DeleteUserView, \
-    PasswordResetRequestView, PasswordResetConfirm, SetNewPasswordView
+    PasswordResetRequestView, PasswordResetConfirm, SetNewPasswordView, BloqueruserView, DebloqueruserView, \
+    GestionUserView
 
 from rest_framework_simplejwt.views import TokenRefreshView
 
@@ -14,6 +15,9 @@ urlpatterns = [
     path('users/<int:pk>/delete/', DeleteUserView.as_view(), name='delete_user'),
     path('password-reset/', PasswordResetRequestView.as_view(), name='password-reset'),
     path('password-reset-confirm/<uidb64>/<token>/', PasswordResetConfirm.as_view(), name='reset-password-confirm'),
-    path('set-new-password/', SetNewPasswordView.as_view(), name='set-new-password'),
+    path('set-new-password/',SetNewPasswordView.as_view(), name='set-new-password'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('bloqueuser/<int:pk>/', BloqueruserView.as_view(), name='bloquer-user'),
+    path('débloqueuser/<int:pk>/', DebloqueruserView.as_view(), name='bloquer-user'),
+    path('gestion-user/<int:pk>/', GestionUserView.as_view(), name='gestion-user'),
     ]
