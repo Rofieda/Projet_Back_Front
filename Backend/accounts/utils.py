3644e61a510d4a884edfae6bdfa90fa1e27b82c2
merouane meriem <lm_merouane@esi.dz>
2024-05-11 10:44:04 +0200

    Update utils.py

--- Backend/accounts/utils.py
+++ Backend/accounts/utils.py
@@ -8,7 +8,7 @@ def send_normal_email(data):
     email = EmailMessage(
         subject=data['email_subject'],
         body=data['email_body'],
-        from_email=settings.EMAIL_HOST_USER,
+        from_email='llmcsquest@gmail.com',
         to=[data['to_email']]
     )
     email.send()
